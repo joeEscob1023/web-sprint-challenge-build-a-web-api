f5598a1288440122e6406d1bbd1f9bf871a77c84
Joe Escobedo <joe_escob1023@live.com>
2021-12-03 13:07:14 -0600

    nodemon is running

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,17 @@
 // Write your "actions" router here!
+const express = require("express");
+const Action = require("./actions-model");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  Action.get(req.query)
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
+const actionRouter = require("./actions/actions-router");
+
 const server = express();
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.use(express.json());
+server.use("/api/actions", actionRouter);
 
 module.exports = server;
